25b2ec3fa53ad4b6c3d942dc6751814039bcfcac
yashyadav-hub <yashyadav.1572003@gmail.com>
2024-11-16 13:45:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(s) {
+    
+    const charCount = {};
+
+   
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    
+    for (let i = 0; i < s.length; i++) {
+        if (charCount[s[i]] === 1) {
+            return s[i]; 
+        }
+    }
+
+    
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
